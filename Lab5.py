9bfad8ba8bd337c38ad779add5cad72454f105b9
pranav11024 <134951581+pranav11024@users.noreply.github.com>
2024-09-04 20:04:52 +0530

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -1,45 +1,88 @@
 import pandas as pd
- import numpy as np
- from sklearn.model_selection import train_test_split
- from sklearn.linear_model import LinearRegression
- from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_percentage_error
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score, calinski_harabasz_score, davies_bouldin_score
+import matplotlib.pyplot as plt
 
-datapath='/content/VSP - Java.xlsx'
-df=pd.read_excel(datapath)
-Xi=df[['Header_and_Main_declaration']]
-X=df[['Header_and_Main_declaration','Incomprehensible_Code','Comprehensible_code_with_logical_errors','Comprehensible_code_with_syntax_errors']]
-y=df[['Final_Marks']]
+# A1: Linear Regression with one attribute
+def linear_regression_one_attr(X_train, y_train):
+    reg = LinearRegression().fit(X_train, y_train)
+    y_train_pred = reg.predict(X_train)
+    return y_train_pred, reg
 
-Xi_train, Xi_test, y_train, y_test = train_test_split(Xi,y,test_size=0.2,random_state=42)
-model=LinearRegression()
-model.fit(Xi_train,y_train)
-y_test_pred=model.predict(Xi_test)
-y_train_pred=model.predict(Xi_train)
+def calculate_metrics(y_true, y_pred):
+    mse = mean_squared_error(y_true, y_pred)
+    rmse = mse ** 0.5
+    mape = mean_absolute_percentage_error(y_true, y_pred)
+    r2 = r2_score(y_true, y_pred)
+    return mse, rmse, mape, r2
 
-def stats(y_test,y_pred):
-  mse=mean_squared_error(y_test,y_pred)
-  rmse=np.sqrt(mse)
+def linear_regression_multi_attr(X_train, y_train):
+    reg = LinearRegression().fit(X_train, y_train)
+    y_train_pred = reg.predict(X_train)
+    return y_train_pred, reg
 
-  y_array_test=np.array(y_test)
-  y_array_test_pred=np.array(y_test_pred)
-  y_array_train=np.array(y_train)
-  y_array_train_pred=np.array(y_test_pred)
-  mape=np.mean(np.abs((y_test-y_pred)/y_test))*100
+def kmeans_clustering(X_train, n_clusters=2):
+    kmeans = KMeans(n_clusters=n_clusters, random_state=0, n_init="auto").fit(X_train)
+    return kmeans.labels_, kmeans.cluster_centers_
 
-  r2=r2_score(y_test,y_pred)
+def clustering_scores(X_train, labels):
+    silhouette = silhouette_score(X_train, labels)
+    ch_score = calinski_harabasz_score(X_train, labels)
+    db_score = davies_bouldin_score(X_train, labels)
+    return silhouette, ch_score, db_score
 
-  print('Mean Squared Error:',mse)
-  print('R-squared:',r2)
-  print('MAPE:',mape,'\n')
+def kmeans_clustering_different_k(X_train, k_values):
+    scores = []
+    for k in k_values:
+        kmeans = KMeans(n_clusters=k, random_state=42).fit(X_train)
+        silhouette = silhouette_score(X_train, kmeans.labels_)
+        ch_score = calinski_harabasz_score(X_train, kmeans.labels_)
+        db_score = davies_bouldin_score(X_train, kmeans.labels_)
+        scores.append((k, silhouette, ch_score, db_score))
+    return scores
 
-stats(y_test,y_test_pred)
-stats(y_train,y_train_pred
+def elbow_plot(X_train, k_values):
+    distortions = []
+    for k in k_values:
+        kmeans = KMeans(n_clusters=k).fit(X_train)
+        distortions.append(kmeans.inertia_)
+    plt.plot(k_values, distortions)
+    plt.xlabel('k')
+    plt.ylabel('Distortion')
+    plt.title('Elbow Method for Optimal k')
+    plt.show()
 
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42)
-model=LinearRegression()
-model.fit(X_train,y_train)
-y_test_pred=model.predict(X_test)
-y_train_pred=model.predict(X_train)
+#Main
+data = pd.read_excel(r"CE_vector_v3.xlsx")
+X_train = data[["Control_Flow_Structures_Vector_0", "Control_Flow_Structures_Vector_1"]]
+y_train = data["Final_Marks"]
+# A1
+y_train_pred_one, reg_one = linear_regression_one_attr(X_train[["Control_Flow_Structures_Vector_0"]], y_train)
+print("A1 - Linear Regression (one attribute) Predictions:", y_train_pred_one)
 
-stats(y_test,y_test_pred)
-stats(y_train,y_train_pred)
+# A2
+mse, rmse, mape, r2 = calculate_metrics(y_train, y_train_pred_one)
+print(f"A2 - Metrics: MSE={mse}, RMSE={rmse}, MAPE={mape}, R2={r2}")
+
+# A3
+y_train_pred_multi, reg_multi = linear_regression_multi_attr(X_train, y_train)
+print("A3 - Linear Regression (multiple attributes) Predictions:", y_train_pred_multi)
+
+# A4
+labels, centers = kmeans_clustering(X_train)
+print("A4 - K-means Clustering Labels:", labels)
+print("A4 - K-means Clustering Centers:", centers)
+
+# A5
+silhouette, ch_score, db_score = clustering_scores(X_train, labels)
+print(f"A5 - Clustering Scores: Silhouette={silhouette}, CH Score={ch_score}, DB Score={db_score}")
+
+# A6
+k_values = range(2, 10)
+clustering_results = kmeans_clustering_different_k(X_train, k_values)
+print("A6 - K-means Clustering Results for different k values:", clustering_results)
+
+# A7
+elbow_plot(X_train, k_values)
